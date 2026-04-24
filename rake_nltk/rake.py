54bedb1cfb6a3a6adafe1936796533829b621332
shubhampawar-ethara <shubham.pawar@ethara.ai>
2026-04-24 13:05:28 +0530

    Commit 0

--- rake_nltk/rake.py
+++ rake_nltk/rake.py
@@ -123,8 +123,7 @@ class Rake:
 
         :param text: Text to extract keywords from, provided as a string.
         """
-        sentences: List[Sentence] = self._tokenize_text_to_sentences(text)
-        self.extract_keywords_from_sentences(sentences)
+        pass
 
     def extract_keywords_from_sentences(self, sentences: List[Sentence]):
         """Method to extract keywords from the list of sentences provided.
@@ -132,10 +131,7 @@ class Rake:
         :param sentences: Text to extraxt keywords from, provided as a list
                           of strings, where each string is a sentence.
         """
-        phrase_list: List[Phrase] = self._generate_phrases(sentences)
-        self._build_frequency_dist(phrase_list)
-        self._build_word_co_occurance_graph(phrase_list)
-        self._build_ranklist(phrase_list)
+        pass
 
     def get_ranked_phrases(self) -> List[Sentence]:
         """Method to fetch ranked keyword strings.
@@ -143,7 +139,7 @@ class Rake:
         :return: List of strings where each string represents an extracted
                  keyword string.
         """
-        return self.ranked_phrases
+        pass
 
     def get_ranked_phrases_with_scores(self) -> List[Tuple[float, Sentence]]:
         """Method to fetch ranked keyword strings along with their scores.
@@ -151,14 +147,14 @@ class Rake:
         :return: List of tuples where each tuple is formed of an extracted
                  keyword string and its score. Ex: (5.68, 'Four Scoures')
         """
-        return self.rank_list
+        pass
 
     def get_word_frequency_distribution(self) -> Dict[Word, int]:
         """Method to fetch the word frequency distribution in the given text.
 
         :return: Dictionary (defaultdict) of the format `word -> frequency`.
         """
-        return self.frequency_dist
+        pass
 
     def get_word_degrees(self) -> Dict[Word, int]:
         """Method to fetch the degree of words in the given text. Degree can be
@@ -167,7 +163,7 @@ class Rake:
 
         :return: Dictionary (defaultdict) of the format `word -> degree`.
         """
-        return self.degree
+        pass
 
     def _tokenize_text_to_sentences(self, text: str) -> List[Sentence]:
         """Tokenizes the given text string into sentences using the configured
@@ -177,7 +173,7 @@ class Rake:
         :param text: String text to tokenize into sentences.
         :return: List of sentences as per the tokenizer used.
         """
-        return self.sentence_tokenizer(text)
+        pass
 
     def _tokenize_sentence_to_words(self, sentence: Sentence) -> List[Word]:
         """Tokenizes the given sentence string into words using the configured
@@ -187,7 +183,7 @@ class Rake:
         :param sentence: String sentence to tokenize into words.
         :return: List of words as per the tokenizer used.
         """
-        return self.word_tokenizer(sentence)
+        pass
 
     def _build_frequency_dist(self, phrase_list: List[Phrase]) -> None:
         """Builds frequency distribution of the words in the given body of text.
@@ -195,7 +191,7 @@ class Rake:
         :param phrase_list: List of List of strings where each sublist is a
                             collection of words which form a contender phrase.
         """
-        self.frequency_dist = Counter(chain.from_iterable(phrase_list))
+        pass
 
     def _build_word_co_occurance_graph(self, phrase_list: List[Phrase]) -> None:
         """Builds the co-occurance graph of words in the given body of text to
@@ -204,18 +200,7 @@ class Rake:
         :param phrase_list: List of List of strings where each sublist is a
                             collection of words which form a contender phrase.
         """
-        co_occurance_graph: DefaultDict[Word, DefaultDict[Word, int]] = defaultdict(lambda: defaultdict(lambda: 0))
-        for phrase in phrase_list:
-            # For each phrase in the phrase list, count co-occurances of the
-            # word with other words in the phrase.
-            #
-            # Note: Keep the co-occurances graph as is, to help facilitate its
-            # use in other creative ways if required later.
-            for (word, coword) in product(phrase, phrase):
-                co_occurance_graph[word][coword] += 1
-        self.degree = defaultdict(lambda: 0)
-        for key in co_occurance_graph:
-            self.degree[key] = sum(co_occurance_graph[key].values())
+        pass
 
     def _build_ranklist(self, phrase_list: List[Phrase]):
         """Method to rank each contender phrase using the formula
@@ -227,19 +212,7 @@ class Rake:
         :param phrase_list: List of List of strings where each sublist is a
                             collection of words which form a contender phrase.
         """
-        self.rank_list = []
-        for phrase in phrase_list:
-            rank = 0.0
-            for word in phrase:
-                if self.metric == Metric.DEGREE_TO_FREQUENCY_RATIO:
-                    rank += 1.0 * self.degree[word] / self.frequency_dist[word]
-                elif self.metric == Metric.WORD_DEGREE:
-                    rank += 1.0 * self.degree[word]
-                else:
-                    rank += 1.0 * self.frequency_dist[word]
-            self.rank_list.append((rank, ' '.join(phrase)))
-        self.rank_list.sort(reverse=True)
-        self.ranked_phrases = [ph[1] for ph in self.rank_list]
+        pass
 
     def _generate_phrases(self, sentences: List[Sentence]) -> List[Phrase]:
         """Method to generate contender phrases given the sentences of the text
@@ -250,26 +223,7 @@ class Rake:
         :return: Set of string tuples where each tuple is a collection
                  of words forming a contender phrase.
         """
-        phrase_list: List[Phrase] = []
-        # Create contender phrases from sentences.
-        for sentence in sentences:
-            word_list: List[Word] = [word.lower() for word in self._tokenize_sentence_to_words(sentence)]
-            phrase_list.extend(self._get_phrase_list_from_words(word_list))
-
-        # Based on user's choice to include or not include repeated phrases
-        # we compute the phrase list and return it. If not including repeated
-        # phrases, we only include the first occurance of the phrase and drop
-        # the rest.
-        if not self.include_repeated_phrases:
-            unique_phrase_tracker: Set[Phrase] = set()
-            non_repeated_phrase_list: List[Phrase] = []
-            for phrase in phrase_list:
-                if phrase not in unique_phrase_tracker:
-                    unique_phrase_tracker.add(phrase)
-                    non_repeated_phrase_list.append(phrase)
-            return non_repeated_phrase_list
-
-        return phrase_list
+        pass
 
     def _get_phrase_list_from_words(self, word_list: List[Word]) -> List[Phrase]:
         """Method to create contender phrases from the list of words that form
@@ -293,6 +247,4 @@ class Rake:
         :return: List of contender phrases honouring phrase length requirements
                  that are formed after dropping stopwords and punctuations.
         """
-        groups = groupby(word_list, lambda x: x not in self.to_ignore)
-        phrases: List[Phrase] = [tuple(group[1]) for group in groups if group[0]]
-        return list(filter(lambda x: self.min_length <= len(x) <= self.max_length, phrases))
+        pass
